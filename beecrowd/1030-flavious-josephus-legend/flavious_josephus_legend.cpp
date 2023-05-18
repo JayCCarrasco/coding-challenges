fe1938eb0bbba8d59824770d637d722c281716df
jayccarrasco <JayCCarrasco@users.noreply.github.com>
2023-05-18 20:58:42 +0200

    Optimizing code

--- beecrowd/1030-flavious-josephus-legend/flavious_josephus_legend.cpp
+++ beecrowd/1030-flavious-josephus-legend/flavious_josephus_legend.cpp
@@ -1,31 +1,34 @@
+//Author: Juan Carlos del Pozo
+//Program: It solves the Flavious Josephus legend problem from Beecrowd coding challenges.
+
+//Libraries
 #include <iostream>
 #include <bitset>
 #include <math.h>
 #include <iomanip>
 #include <iterator>
-/*
-#include <string.h>
 
-
-#include <algorithm>
-*/
-
+//Main
 int main() {
- 
+
+    //Declaration of variables 
     int cases, number, step = 1;
     int j = 0;
     int alive = 0;
     int casesTemp = 0;
 
+    //Input number of cases
     std::cin>>cases;
 
     while(casesTemp<cases){
 
+        //Input number of persons and steps
         std::cin>>number>>step;
 
+        //Declaration of dynamic arrays
         int* array1= new int[number];
-        int* array2 = new int[number];
 
+        //Fill array1
         for (int i = 0; i < number; i++){
             array1[i] = i+1;
         }
@@ -33,60 +36,40 @@ int main() {
         int i = -1;
         int numTotal = 0;
 
+        //Each time someone is murder numTotal++
+        //When number-1 stops-->Person alive
         while(numTotal != number-1){
             i++;
+            //Reset loop
             if (i > number-1){
                 i = 0;
             }
+            //j does not count person murdered
             if (array1[i] != 0 && j != step){
                 j++;
             }
-            //std::cout<<"antes"<<array1[i]<<std::endl;
+            //Murder person
             if (j == step){
                 array1[i]=0;
                 j=0;
                 numTotal++;
-            }
-            //std::cout<<"despues"<<array1[i]<<std::endl;     
+            }   
         }
 
-        //std::cout<<"el array queda: "<<std::endl;
+        //show results
         for (int i = 0; i < number; i++){
-            //std::cout<<array1[i]<<" ";
             if (array1[i]!=0){
                 std::cout<<"Case "<<casesTemp+1<<": ";
                 std::cout<<array1[i]<<std::endl;;
             }
-        }
-
-        //std::cout<<"The survivor is: "<<alive<<std::endl;
+        }     
         
-        
-
+        //VERY IMPORTANT!!! delete array from heap memory
         delete array1;
         array1 = nullptr;
-        delete array2;
-        array2 = nullptr;
-
+        
         casesTemp++;
     }
-
-
-
-    /*
-    std::cin>>cases;
-
-    
-
-    while (casesTemp<cases){
-        std::cin>>number>>step;
-        for (int i = 0; i < number; i++){
-            
-        }
-        
-    }
-
-    */
 
     return 0;
 }
